adfafe4c1cddfb09573617303e032254ae3f9183
Max Froumentin <max@lapin-bleu.net>
2013-04-19 16:44:59 +0100

    use block instead of step

--- flow.js
+++ flow.js
@@ -8,33 +8,33 @@ flow.data = {};
 flow.buildHistoryFromData = function() {
   console.log("buildHistoryFromData");
   // from the existing state of the data, recalculate the history
-  var currentStepId = "start", i;
+  var currentBlockId = "start", i;
   flow.history = [];
   console.log("history");
   while (true) {
-    flow.history.push(currentStepId);
-    console.log("adding to history: "+currentStepId);
+    flow.history.push(currentBlockId);
+    console.log("adding to history: "+currentBlockId);
 
-    state = $("#"+currentStepId);
+    state = $("#"+currentBlockId);
 
-    if (flow.data[currentStepId]) {
+    if (flow.data[currentBlockId]) {
 
-      // this step has data, move to next step
-      transitions = $("#"+currentStepId+" transition");
+      // this block has data, move to next block
+      transitions = $("#"+currentBlockId+" transition");
       for (i in transitions) {
         t = transitions[i];
         if ($(t).attr('if')===undefined) {
-          nextStepId = $(t).attr("target");
+          nextBlockId = $(t).attr("target");
           break;
         } else {
           if (eval($(t).attr("if"))) {
-            nextStepId = $(t).attr("target");
+            nextBlockId = $(t).attr("target");
             break;
           }
         }
       }
-      if (nextStepId) {
-        currentStepId = nextStepId;
+      if (nextBlockId) {
+        currentBlockId = nextBlockId;
       } else {
         console.log("error. No transition found");
         return;
@@ -54,7 +54,7 @@ flow.formatAnswer = function(answer) {
       alert("Object.keys not available. You're probably using an old browser.");
   }
   if (keys.length === 1) {
-    // if there's only one form item in the step, don't show the answer's key, as the step's title (h2) suffices
+    // if there's only one form item in the block, don't show the answer's key, as the block's title (h2) suffices
     result.push(answer[keys[0]]);
   } else {
     for (key in answer) {
@@ -65,53 +65,53 @@ flow.formatAnswer = function(answer) {
 };
 
 flow.buildViewFromHistory = function() {
-  // walk down the history and write the data of each step
+  // walk down the history and write the data of each block
   var items = [], i;
   $("#summary").html();
 
   // Summary of previous answers
-  for (i=0; i<flow.history.length - 1; i++) { // history includes the current step. Don't include it.
-    stepId = flow.history[i];
-    items.push("<li class='done'><h3 class='question'>"+flow.data[stepId].question+"</h3><div class='answer'>"+flow.formatAnswer(flow.data[stepId]['answer'])+"</div><p class='undo' onclick='flow.deleteData(\""+stepId+"\")' href='#'>Change this answer</p></li>");
+  for (i=0; i<flow.history.length - 1; i++) { // history includes the current block. Don't include it.
+    blockId = flow.history[i];
+    items.push("<li class='done'><h3 class='question'>"+flow.data[blockId].question+"</h3><div class='answer'>"+flow.formatAnswer(flow.data[blockId]['answer'])+"</div><p class='undo' onclick='flow.deleteData(\""+blockId+"\")' href='#'>Change this answer</p></li>");
   }
   $("#summary").html(items.join(''));
   $(".done-questions").css("display","block");
 
   // current state
-  stepId = flow.history[flow.history.length-1];
-  $("div.step > button:submit").off("click");
-  $("#"+stepId+" > button:submit").on("click", function() {
+  blockId = flow.history[flow.history.length-1];
+  $("div.block > button:submit").off("click");
+  $("#"+blockId+" > button:submit").on("click", function() {
     var i;
     // update data model
-    console.log("creating data["+stepId+"]");
-    flow.data[stepId] = {"question":$("#"+stepId+" h2").text(), "answer":{}};
-    $("#"+stepId+" textarea").each(function(index, textarea) { flow.data[stepId]['answer'][textarea.name] = textarea.value; });
-    $("#"+stepId+" input:text").each(function(index, text) { flow.data[stepId]['answer'][text.name] = text.value; });
-    $("#"+stepId+" input:radio:checked").each(function(index, radioButton) {
-      flow.data[stepId]['answer'][radioButton.name] = radioButton.value;
+    console.log("creating data["+blockId+"]");
+    flow.data[blockId] = {"question":$("#"+blockId+" h2").text(), "answer":{}};
+    $("#"+blockId+" textarea").each(function(index, textarea) { flow.data[blockId]['answer'][textarea.name] = textarea.value; });
+    $("#"+blockId+" input:text").each(function(index, text) { flow.data[blockId]['answer'][text.name] = text.value; });
+    $("#"+blockId+" input:radio:checked").each(function(index, radioButton) {
+      flow.data[blockId]['answer'][radioButton.name] = radioButton.value;
     });
     flow.buildHistoryFromData();
     flow.buildViewFromHistory();
   });
-  $("div.step").css("display","none");
-  flow.showStep(stepId);
+  $("div.block").css("display","none");
+  flow.showBlock(blockId);
 };
 
 
-flow.deleteData = function(stepId) {
-  delete flow.data[stepId];
+flow.deleteData = function(blockId) {
+  delete flow.data[blockId];
   flow.start();
 };
 
-flow.showStep = function(stepId) {
-  var thisStep = $("#"+stepId);
-  var conditionalText = $("#"+stepId+" p[cond], #"+stepId+" label[cond]");
+flow.showBlock = function(blockId) {
+  var thisBlock = $("#"+blockId);
+  var conditionalText = $("#"+blockId+" p[cond], #"+blockId+" label[cond]");
 
   for (i=0; i<conditionalText.length;i++) {
     item = conditionalText[i];
     item.style.display = eval(item.getAttribute("cond")) ? "block" : "none";
   }
-  thisStep.css("display", "block");
+  thisBlock.css("display", "block");
 };
 
 
