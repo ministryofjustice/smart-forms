32bb01876f2ce83161afadff72a6917763c93dd4
Max Froumentin <max@lapin-bleu.net>
2013-04-29 17:15:29 +0100

    encapsulate

--- flow.js
+++ flow.js
@@ -1,133 +1,132 @@
+/* exported flow */
 /* jshint evil: true */
 
 var $, validate;
-var flow = {};
-var i;
 
-flow.history = [];
-flow.data = {};
-
-flow.buildHistoryFromData = function() {
+var flow = (function () {
   "use strict";
-  // from the existing state of the data, recalculate the history
-  var currentBlockId = "start", i, state, transitions, t, nextBlockId;
-  flow.history = [];
-  while (true) {
-    flow.history.push(currentBlockId);
+  var i, history = [];
 
-    state = $("#"+currentBlockId);
+  function buildHistoryFromData() {
+    // from the existing state of the data, recalculate the history
+    var currentBlockId = "start", i, state, transitions, t, nextBlockId;
+    history = [];
+    while (true) {
+      history.push(currentBlockId);
+      state = $("#"+currentBlockId);
 
-    if (flow.data[currentBlockId]) {
+      if (flow.data[currentBlockId]) {
 
-      // this block has data, move to next block
-      transitions = $("#"+currentBlockId+" transition");
-      for (i in transitions) {
-        if (transitions.hasOwnProperty(i)) {
-          t = transitions[i];
-          if ($(t).attr("if")===undefined) {
-            nextBlockId = $(t).attr("target");
-            break;
-          } else {
-            if (eval($(t).attr("if"))) {
+        // this block has data, move to next block
+        transitions = $("#"+currentBlockId+" transition");
+        for (i in transitions) {
+          if (transitions.hasOwnProperty(i)) {
+            t = transitions[i];
+            if ($(t).attr("if")===undefined) {
               nextBlockId = $(t).attr("target");
               break;
+            } else {
+              if (eval($(t).attr("if"))) {
+                nextBlockId = $(t).attr("target");
+                break;
+              }
             }
           }
         }
-      }
-      if (nextBlockId) {
-        currentBlockId = nextBlockId;
+        if (nextBlockId) {
+          currentBlockId = nextBlockId;
+        } else {
+          console.log("error. No transition found");
+          return;
+        }
       } else {
-        console.log("error. No transition found");
+        // no data for this state. Move to transition
+        transitions = $("#"+currentBlockId+" transition");
+        t = transitions[0];
+        nextBlockId = $(t).attr("target");
         return;
       }
+    }
+  }
+
+  function formatAnswer(answer) {
+    var key, keys=[], result=[];
+    if (Object.keys) {
+      keys = Object.keys(answer);
     } else {
-      // no data for this state. Move to transition
-      transitions = $("#"+currentBlockId+" transition");
-      t = transitions[0];
-      nextBlockId = $(t).attr("target");
-      return;
+      alert("Object.keys not available. You're probably using an old browser.");
     }
-  }
-};
-
-flow.formatAnswer = function(answer) {
-  "use strict";
-  var key, keys=[], result=[];
-  if (Object.keys) {
-    keys = Object.keys(answer);
-  } else {
-    alert("Object.keys not available. You're probably using an old browser.");
-  }
-  if (keys.length === 1) {
-    // if there's only one form item in the block, don't show the answer's key, as the block's title (h2) suffices
-    result.push(answer[keys[0]]);
-  } else {
-    for (key in answer) {
-      result.push(key+": "+answer[key]);
+    if (keys.length === 1) {
+      // if there's only one form item in the block, don't show the answer's key, as the block's title (h2) suffices
+      result.push(answer[keys[0]]);
+    } else {
+      for (key in answer) {
+        if (answer.hasOwnProperty(key)) {
+          result.push(key+": "+answer[key]);
+        }
+      }
     }
+    return result.join("<br/>");
   }
-  return result.join("<br/>");
-};
 
-flow.buildViewFromHistory = function() {
-  "use strict";
-  // walk down the history and write the data of each block
-  var items = [], i, blockId;
-  $("#summary").html();
+  function buildViewFromHistory() {
+    // walk down the history and write the data of each block
+    var items = [], i, blockId;
+    $("#summary").html();
 
-  // Summary of previous answers
-  for (i=0; i<flow.history.length - 1; i++) { // history includes the current block. Don't include it.
-    blockId = flow.history[i];
-    items.push("<li class='done'><h3 class='question'>"+flow.data[blockId].question+"</h3><div class='answer'>"+flow.formatAnswer(flow.data[blockId].answer)+"</div><p class='undo' onclick='flow.deleteData(\""+blockId+"\")' href='#'>Change this answer</p></li>");
-  }
-  $("#summary").html(items.join(''));
-  $(".done-questions").css("display","block");
-
-  // current state
-  blockId = flow.history[flow.history.length-1];
-  $(".block input[type='button']").off("click");
-  $("#"+blockId+" input[type='button']").on("click", function() {
-    if (validate.validateForm(blockId)) {
-      // update data model
-      flow.data[blockId] = {"question":$("#"+blockId+" h2").text(), "answer":{}};
-      $("#"+blockId+" textarea").each(function(index, textarea) { flow.data[blockId].answer[textarea.name] = textarea.value; });
-      $("#"+blockId+" input:text").each(function(index, text) { flow.data[blockId].answer[text.name] = text.value; });
-      $("#"+blockId+" input:radio:checked").each(function(index, radioButton) {
-        flow.data[blockId].answer[radioButton.name] = radioButton.value;
-      });
-      flow.buildHistoryFromData();
-      flow.buildViewFromHistory();
+    // Summary of previous answers
+    for (i=0; i<history.length - 1; i++) { // history includes the current block. Don't include it.
+      blockId = history[i];
+      items.push("<li class='done'><h3 class='question'>"+flow.data[blockId].question+"</h3><div class='answer'>"+formatAnswer(flow.data[blockId].answer)+"</div><p class='undo' onclick='deleteData(\""+blockId+"\")' href='#'>Change this answer</p></li>");
     }
-  });
-  $(".block").css("display","none");
-  flow.showBlock(blockId);
-};
+    $("#summary").html(items.join(''));
+    $(".done-questions").css("display","block");
 
-
-flow.deleteData = function(blockId) {
-  "use strict";
-  delete flow.data[blockId];
-  flow.start();
-};
-
-flow.showBlock = function(blockId) {
-  "use strict";
-  var thisBlock = $("#"+blockId),
-      conditionalText = $("#"+blockId+" p[cond], #"+blockId+" label[cond]"),
-      item;
-
-  for (i=0; i<conditionalText.length;i++) {
-    item = conditionalText[i];
-    item.style.display = eval(item.getAttribute("cond")) ? "block" : "none";
+    // current state
+    blockId = history[history.length-1];
+    $(".block input[type='button']").off("click");
+    $("#"+blockId+" input[type='button']").on("click", function() {
+      if (validate.validateForm(blockId)) {
+        // update data model
+        flow.data[blockId] = {"question":$("#"+blockId+" h2").text(), "answer":{}};
+        $("#"+blockId+" textarea").each(function(index, textarea) { flow.data[blockId].answer[textarea.name] = textarea.value; });
+        $("#"+blockId+" input:text").each(function(index, text) { flow.data[blockId].answer[text.name] = text.value; });
+        $("#"+blockId+" input:radio:checked").each(function(index, radioButton) {
+          flow.data[blockId].answer[radioButton.name] = radioButton.value;
+        });
+        buildHistoryFromData();
+        buildViewFromHistory();
+      }
+    });
+    $(".block").css("display","none");
+    showBlock(blockId);
   }
-  thisBlock.css("display", "block");
-};
 
 
-flow.start = function() {
-  "use strict";
-  flow.buildHistoryFromData();
-  flow.buildViewFromHistory();
-};
+  function deleteData(blockId) {
+    delete flow.data[blockId];
+    flow.start();
+  }
 
+  function showBlock(blockId) {
+    var thisBlock = $("#"+blockId),
+        conditionalText = $("#"+blockId+" p[cond], #"+blockId+" label[cond]"),
+        item;
+
+    for (i=0; i<conditionalText.length;i++) {
+      item = conditionalText[i];
+      item.style.display = eval(item.getAttribute("cond")) ? "block" : "none";
+    }
+    thisBlock.css("display", "block");
+  }
+
+
+  return {
+    "data": {},
+
+    "start": function() {
+      buildHistoryFromData();
+      buildViewFromHistory();
+    }
+  };
+})();
--- validate.js
+++ validate.js
@@ -1,34 +1,41 @@
-var $, validate = {};
+/* exported validate */
+var $;
 
-validate.postcodeRe = /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/;
-validate.dateRe = /^\d{2}\/\d{2}\/\d{4}/;
-validate.validateForm = function (formId) {
+var validate = (function () {
   "use strict";
+  var
+    postcodeRe = /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/,
+    dateRe = /^\d{2}\/\d{2}\/\d{4}/;
 
-  var isValid = true, inputs = $("#" + formId + " input"), i, inputElement, inputElementName;
-  for (i=0; i < inputs.length; i++) {
-    inputElement = $(inputs[i]);
-    inputElementName = inputElement.attr("name");
+  return {
+    "validateForm": function (formId) {
 
-    if (!inputElement.hasClass("valid-optional") && inputElement.val() === "") {
-      alert("The field called '" + inputElementName + "' must be filled out.");
-      isValid = false;
-      break;
+      var isValid = true, inputs = $("#" + formId + " input, #" + formId + " textarea"), i, inputElement, inputElementName;
+      for (i=0; i < inputs.length; i++) {
+        inputElement = $(inputs[i]);
+        inputElementName = inputElement.attr("name");
+
+        if (!inputElement.hasClass("valid-optional") && inputElement.val() === "") {
+          alert("The field called '" + inputElementName + "' must be filled out.");
+          isValid = false;
+          break;
+        }
+
+        // Validate postcode
+        if (inputElement.hasClass("valid-postcode") && !postcodeRe.test(inputElement.val())) {
+          alert("Invalid value for: \"" + inputElementName + "\"");
+          isValid = false;
+          break;
+        }
+
+        // validate date
+        if (inputElement.hasClass("valid-date") && !dateRe.test(inputElement.val())) {
+          alert("Invalid value for: \""+inputElementName + "\"");
+          isValid = false;
+          break;
+        }
+      }
+      return isValid;
     }
-
-    // Validate postcode
-    if (inputElement.hasClass("valid-postcode") && !validate.postcodeRe.test(inputElement.val())) {
-      alert("Invalid value for: \"" + inputElementName + "\"");
-      isValid = false;
-      break;
-    }
-
-    // validate date
-    if (inputElement.hasClass("valid-date") && !validate.dateRe.test(inputElement.val())) {
-      alert("Invalid value for: \""+inputElementName + "\"");
-      isValid = false;
-      break;
-    }
-  }
-  return isValid;
-};
+  };
+})();
