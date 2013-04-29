f7297f97455406015b0e0e72281e8dec991747fb
Max Froumentin <max@lapin-bleu.net>
2013-04-29 13:50:20 +0100

    Fix variable name mixup

--- validate.js
+++ validate.js
@@ -4,11 +4,10 @@ validate.postcodeRe = /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY
 validate.dateRe = /^\d{2}\/\d{2}\/\d{4}/;
 
 validate.validateForm = function(formId) {
-  var isValid = true, inputs = $("#"+formId+" input"), i, regexp, inputElementName;
+  var isValid = true, inputs = $("#"+formId+" input"), i, regexp, inputElement, inputElementName;
   for (i=0; i<inputs.length; i++) {
     inputElement = $(inputs[i]);
-    inputElementName = inputElementName;
-    console.log(inputElement);
+    inputElementName = inputElement.attr('name');
 
     if (!inputElement.hasClass("valid-optional") && inputElement.val() === "") {
       alert("The field called \"" + inputElementName + "\" must be filled out.");
